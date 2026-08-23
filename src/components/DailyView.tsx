f922d64580c62329767d5ebd5e63ea94af4f7375
Mieqkk <138146845+Mieqk@users.noreply.github.com>
2026-08-24 03:59:34 +1000

    Update DailyView.tsx

--- src/components/DailyView.tsx
+++ src/components/DailyView.tsx
@@ -34,7 +34,7 @@ import {
 function generateUUID() {
   if (typeof crypto !== 'undefined' && crypto.randomUUID) {
     return crypto.randomUUID();
-.  }
+  }
   return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, c => {
     const r = Math.random() * 16 | 0;
     const v = c === 'x' ? r : (r & 0x3 | 0x8);
