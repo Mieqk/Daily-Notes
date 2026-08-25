50b71d61b60d04fffad230a8b1f0344c021acd52
Mieqkk <138146845+Mieqk@users.noreply.github.com>
2026-08-26 04:27:10 +1000

    Update FriendsView.tsx

--- src/components/FriendsView.tsx
+++ src/components/FriendsView.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import type { Lang } from "../i18n";
 import { supabase } from "../lib/supabase";
 
